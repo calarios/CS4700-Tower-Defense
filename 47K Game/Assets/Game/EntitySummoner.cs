59ff8d287720844b5ae524a4ce728a6a073fef7b
calarios <carlosalarios626@gmail.com>
2024-10-30 16:53:21 -0700

    Spawning system 10302024
    With the help of a guide, created a spawning/despawning system, must test this out more.

---
+++ 47K Game/Assets/Game/EntitySummoner.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EntitySummoner : MonoBehaviour
+{
+
+    public static List<Enemy> EnemiesInGame;
+    public static Dictionary<int, GameObject> EnemyPrefabs;
+    public static Dictionary<int, Queue<Enemy>> EnemyObjectPools;
+
+    private static bool IsInitialized;
+
+    public static void Init()
+    {
+        if(!IsInitialized)
+        {
+            EnemyPrefabs = new Dictionary<int, GameObject>();
+            EnemyObjectPools = new Dictionary<int, Queue<Enemy>>();
+            EnemiesInGame = new List<Enemy>();
+
+            EnemySummonData[] Enemies = Resources.LoadAll<EnemySummonData>("Enemies");
+            Debug.Log(Enemies[0].name);
+
+            foreach(EnemySummonData enemy in Enemies)
+            {
+                EnemyPrefabs.Add(enemy.EnemyID, enemy.EnemyPrefab);
+                EnemyObjectPools.Add(enemy.EnemyID, new Queue<Enemy>());
+            }
+
+            IsInitialized = true;
+        }
+        else
+        {
+            Debug.Log("ENTITYSUMMONER: THIS CLASS IS ALREADY INITIALIZED");
+        }
+    }
+
+    public static Enemy SummonEnemy(int EnemyID)
+    {
+        Enemy SummonedEnemy = null;
+
+        if(EnemyPrefabs.ContainsKey(EnemyID))
+        {
+            Queue<Enemy> ReferencedQueue = EnemyObjectPools[EnemyID];
+            if(ReferencedQueue.Count > 0)
+            {
+                //Dequeue Enemy and iniialized
+
+                SummonedEnemy = ReferencedQueue.Dequeue();
+                SummonedEnemy.Init();
+
+                SummonedEnemy.gameObject.SetActive(true);
+            }
+            else
+            {
+                //Instantiate new instance of enemy and initialized
+                GameObject NewEnemy = Instantiate(EnemyPrefabs[EnemyID], Vector3.zero, Quaternion.identity);
+                SummonedEnemy = NewEnemy.GetComponent<Enemy>();
+                SummonedEnemy.Init();
+            }
+        }
+        else
+        {
+            Debug.Log($"ENTITYSUMMONER: ENEMY WITH ID OF {EnemyID} DOES NOT EXIST!");
+            return null;
+        }
+        EnemiesInGame.Add(SummonedEnemy);
+        SummonedEnemy.ID = EnemyID;
+        return SummonedEnemy;
+    }
+
+    public static void RemoveEnemy(Enemy EnemyToRemove)
+    {
+        EnemyObjectPools[EnemyToRemove.ID].Enqueue(EnemyToRemove);
+        EnemyToRemove.gameObject.SetActive(false);
+        EnemiesInGame.Remove(EnemyToRemove);
+    }
+}
--- 47K Game/Assets/Game/GameLoopManager.cs
+++ 47K Game/Assets/Game/GameLoopManager.cs
@@ -5,12 +5,32 @@ using UnityEngine;
 public class GameLoopManager : MonoBehaviour
 {
 
+    private static Queue<int> EnemyIDsToSummon;
+
     public bool LoopShouldEnd;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        EnemyIDsToSummon = new Queue<int>();
+        EntitySummoner.Init();
+
+        StartCoroutine(GameLoop());
+        InvokeRepeating("SummonTest", 0f, 1f);
+        InvokeRepeating("RemoveTest", 0f, 05f);
+    }
+
+    void RemoveTest()
+    {
+        if(EntitySummoner.EnemiesInGame.Count>0)
+        {
+            EntitySummoner.RemoveEnemy(EntitySummoner.EnemiesInGame[Random.Range(0, EntitySummoner.EnemiesInGame.Count)]);
+        }
+    }
+
+    void SummonTest()
+    {
+        EnqueEnemyIDToSummon(1);
     }
 
     IEnumerator GameLoop()
@@ -18,6 +38,14 @@ public class GameLoopManager : MonoBehaviour
         while (LoopShouldEnd == false)
         {
             //Spawn Enemies
+
+            if(EnemyIDsToSummon.Count> 0)
+            {
+                for(int i = 0; i< EnemyIDsToSummon.Count;i++)
+                {
+                    EntitySummoner.SummonEnemy(EnemyIDsToSummon.Dequeue());
+                }
+            }
 
             //Spawn Towers
 
@@ -37,9 +65,8 @@ public class GameLoopManager : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void EnqueEnemyIDToSummon(int ID)
     {
-        
+        EnemyIDsToSummon.Enqueue(ID);
     }
 }
